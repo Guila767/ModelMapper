76c97f347be432418e2f392bcc5f608083c9cfe1
Guilherme Ladario <Gladario@zaave.com.br>
2023-01-09 19:18:02 -0300

    Added generator_test

---
+++ model_mapper/test/generator_test.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'generator_test.dart';
+
+// **************************************************************************
+// ModelMapperGenerator
+// **************************************************************************
+
+class $ModelTest_ModelFactory implements IModelFactory<ModelTest> {
+  IModelBase Function(Map<String, dynamic> map) get fromMap =>
+      ModelTest.fromJson;
+}
+
+// **************************************************************************
+// FactoryMapperGenerator
+// **************************************************************************
+
+IModelFactory<T> $TestFactory_get<T extends IModelBase>() {
+  switch (T) {
+    case ModelTest:
+      return $ModelTest_ModelFactory() as IModelFactory<T>;
+    default:
+      throw 'ModelMapper Error';
+  }
+}
