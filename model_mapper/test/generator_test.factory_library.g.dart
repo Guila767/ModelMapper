a1e2d9aed9f729cda4495c996db5bd088bbd86ec
Guilherme de Massena Ladario <gui.ladario@hotmail.com>
2023-01-10 20:37:29 -0300

    fix generating wrong export paths for internal assets in the package

---
+++ model_mapper/test/generator_test.factory_library.g.dart
@@ -0,0 +1,7 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// FactoryLibraryGenerator
+// **************************************************************************
+
+export '../test/generator_test.dart';
